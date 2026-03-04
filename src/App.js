f6b66e81deed869fe759ecadfba9ff3c83493cd1
VoaybeDev <mbinanafana1@gmail.com>
2026-03-04 23:50:48 +0300

    Ajout du portfolio en version initiale

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 // src/App.js
-import React, { Suspense } from 'react';
+
+import React, { Suspense } from 'react'; // Tous les imports en haut
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import ErrorBoundary from './components/ErrorBoundary'; // ✅ Déplacé ici
 
 // Chargement asynchrone des composants
 const Projects = React.lazy(() => import('./components/Projects'));
 const Contact = React.lazy(() => import('./components/Contact'));
 const Skills = React.lazy(() => import('./components/Skills'));
-
-// Gestion des erreurs avec ErrorBoundary
-import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
